2b2c6c140f17e93cffbb4c057f26e5bdb6088b90
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-10-09 11:12:00 +0700

    seed utk default user dan default opd

---
+++ seeders/20231009022729-default-user.js
@@ -0,0 +1,38 @@
+'use strict';
+const { hashPassword } = require('../helpers/bcrypt')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Pegawais', [{
+      nama: 'admin',
+      nip: '123456789012345678',
+      password: hashPassword('enotulen_kota_madiun'),
+      pangkat: 'super_admin',
+      nama_pangkat: 'super_admin',
+      jabatan: 'super_admin',
+      role: 1,
+      kode_opd: '1234567890',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
