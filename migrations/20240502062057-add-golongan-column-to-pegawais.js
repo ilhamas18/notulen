fbc38bcb333a13d18b9559f8e36c99ad770c7de0
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-05-02 13:39:01 +0700

    migrasi fix golongan

---
+++ migrations/20240502062057-add-golongan-column-to-pegawais.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addColumn(
+        "Pegawais",
+        "golongan",
+        {
+          type: Sequelize.DataTypes.STRING,
+        },
+        { transaction },
+      );
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeColumn("Pegawais", "golongan", {
+        transaction,
+      });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+};
