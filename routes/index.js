268d3fee7a0ef71f83de1734be421add5c8e53e6
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-05-02 11:26:43 +0700

    cek status backend di /health

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,14 @@ router.use('/notulen', NotulenRouter);
 router.use('/laporan', LaporanRouter);
 router.use('/tagging', TaggingRouter);
 router.use('/upload', UploadRouter);
+router.get('/health', (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date()
+  }
+
+  res.status(200).send(data);
+})
 
 module.exports = router;
